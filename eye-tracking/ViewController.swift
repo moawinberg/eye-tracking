bc4cd29a9c6d94a46b1549f879f7f65877f655fd
Moa <moa.winberg.1@gmail.com>
2021-01-22 10:01:25 +0100

    add hittest and change indicator

--- eye-tracking/ViewController.swift
+++ eye-tracking/ViewController.swift
@@ -10,75 +10,76 @@ import SceneKit
 import ARKit
 import WebKit
 
-extension matrix_float4x4 {
-    func position() -> SCNVector3 {
-        return SCNVector3(columns.3.x, columns.3.y, columns.3.z)
-    }
-}
-
-
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - outlets
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var gazeIndicator: UIImageView!
     
     // MARK: - variables
-    var phonePointsWidth = 414;
-    var phonePointsHeight = 896;
+    var phonePointsWidth = CGFloat(414);
+    var phonePointsHeight = CGFloat(896);
+    var phoneWidth = CGFloat(0.1509)
+    var phoneHeight = CGFloat(0.0757)
     
-    var virtualPhone: SCNNode = SCNNode()
-    var virtualScreen: SCNNode = {
-        let plane = SCNPlane(width: 1, height: 1)
-        return SCNNode(geometry: plane)
+    // Set target at 2 meters away from the center of eyeballs to create segment vector
+    var gazeTargetLeftEye: SCNNode = {
+        let node = SCNNode()
+        node.position.z = 2
+        return node
+    }()
+    
+    var gazeTargetRightEye: SCNNode = {
+        let node = SCNNode()
+        node.position.z = 2
+        return node
     }()
     
     var leftEye: SCNNode = {
-        let cylinder = SCNCylinder(radius: 0.002, height: 0.1)
-        cylinder.firstMaterial?.diffuse.contents = UIColor.red
-        return SCNNode(geometry: cylinder)
+        let node = SCNNode()
+        node.eulerAngles.x = -.pi / 2 // rotate x angle (pitch) -90 degrees to point at device
+        node.position.z = 0.1
+        let parentNode = SCNNode()
+        parentNode.addChildNode(node)
+        return parentNode
     }()
     
     var rightEye: SCNNode = {
-        let cylinder = SCNCylinder(radius: 0.002, height: 0.1)
-        cylinder.firstMaterial?.diffuse.contents = UIColor.green
-        return SCNNode(geometry: cylinder)
+        let node = SCNNode()
+        node.eulerAngles.x = -.pi / 2
+        node.position.z = 0.1
+        let parentNode = SCNNode()
+        parentNode.addChildNode(node)
+        return parentNode
     }()
-    
-    // algebra from: https://github.com/andrewzimmer906/HeatMapEyeTracking?fbclid=IwAR2R663PglsButlR0d-tT9egU3UhTYi4EWIrgs50wRTt2SsfdyfNnJNrPGo
-    let eyeRotationMatrix: matrix_float4x4 =
-        simd_float4x4(
-            SCNMatrix4Mult(
-                SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0), SCNMatrix4MakeTranslation(0, 0, 0.1/2)
-            )
-        )
+
+    var virtualPhoneNode: SCNNode = {
+        let geometry = SCNPlane(width: 1, height: 1)
+        geometry.firstMaterial?.isDoubleSided = true
+        geometry.firstMaterial?.fillMode = .fill
+        geometry.firstMaterial?.diffuse.contents = UIColor.blue
+
+        let node = SCNNode()
+        node.geometry = geometry
+        return node
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // webView.load(URLRequest(url: URL(string: "https://www.lipsum.com/")!))
      
         sceneView.delegate = self
         sceneView.automaticallyUpdatesLighting = true
         UIApplication.shared.isIdleTimerDisabled = true
         
-        // add virtual screen to virtual phone
-        virtualPhone.addChildNode(virtualScreen)
-        // add virtual phone to scene
-        sceneView.scene.rootNode.addChildNode(virtualPhone)
-        
-
+        sceneView.pointOfView?.addChildNode(virtualPhoneNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
         guard ARFaceTrackingConfiguration.isSupported else { return }
-        
         let configuration = ARFaceTrackingConfiguration()
-        configuration.isLightEstimationEnabled = true
-        
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
@@ -97,44 +98,73 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let node = SCNNode(geometry: faceMesh)
         node.geometry?.firstMaterial?.fillMode = .lines
         
+        // add nodes to face
         node.addChildNode(leftEye)
         node.addChildNode(rightEye)
+        
+        // add target to eyes
+        leftEye.addChildNode(gazeTargetLeftEye)
+        rightEye.addChildNode(gazeTargetRightEye)
         
         return node
     }
     
-    func avgDistance(_ node1: SCNNode, _ node2: SCNNode) -> Float {
+    func averageDistance() {
         // Distance of the eyes to the camera don't have to subtract because the center of worldPosition (camera) is (0,0,0)
-        let distanceL = node1.worldPosition
-        let distanceR = node2.worldPosition
+        let distanceL = leftEye.worldPosition
+        let distanceR = rightEye.worldPosition
         
+        // euqludian distance
         let distanceLLength = sqrt(distanceL.x*distanceL.x + distanceL.y*distanceL.y + distanceL.z*distanceL.z)
         let distanceRLength = sqrt(distanceR.x*distanceR.x + distanceR.y*distanceR.y + distanceR.z*distanceR.z)
         
-        return (distanceLLength + distanceRLength) / 2
-    }
-    
-    // update face mesh when face changes
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        if let faceAnchor = anchor as? ARFaceAnchor, let faceGeometry = node.geometry as? ARSCNFaceGeometry {
-            faceGeometry.update(from: faceAnchor.geometry)
+        let distance = (distanceLLength + distanceRLength) / 2
 
-            leftEye.simdTransform = faceAnchor.leftEyeTransform * eyeRotationMatrix;
-            rightEye.simdTransform = faceAnchor.rightEyeTransform * eyeRotationMatrix;
-            
-            let distance = avgDistance(leftEye, rightEye)
-            
-            DispatchQueue.main.async{
-                self.distanceLabel.text = "\(Int(round(distance * 100))) cm"
-            }
+        DispatchQueue.main.async{
+            self.distanceLabel.text = "\(Int(round(distance * 100))) cm"
         }
     }
     
-    // called once per frame
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        virtualPhone.transform = (sceneView.pointOfView?.transform)!
+    func hitTest() {
+        guard let rightEyeHitTestResults = self.virtualPhoneNode.hitTestWithSegment(
+                from: self.gazeTargetRightEye.worldPosition,
+                to: self.rightEye.worldPosition,
+                options: nil
+        ).first else { return }
+        
+        guard let leftEyeHitTestResults = self.virtualPhoneNode.hitTestWithSegment(
+                from: self.gazeTargetLeftEye.worldPosition,
+                to: self.leftEye.worldPosition,
+                options: nil
+        ).first else { return }
+        
+        let rightEyeX = CGFloat(rightEyeHitTestResults.localCoordinates.x) / (phoneWidth / 2) * phonePointsWidth
+        let rightEyeY = CGFloat(rightEyeHitTestResults.localCoordinates.y) / (phoneHeight / 2) * phonePointsHeight
+        
+        let leftEyeX = CGFloat(leftEyeHitTestResults.localCoordinates.x) / (phoneWidth / 2) * phonePointsWidth
+        let leftEyeY = CGFloat(leftEyeHitTestResults.localCoordinates.y) / (phoneHeight / 2) * phonePointsHeight
+        
+        let avgX = (rightEyeX + leftEyeX) / 2
+        let avgY = (rightEyeY + leftEyeY) / 2
+
+        DispatchQueue.main.async{
+            self.gazeIndicator.center = CGPoint(x: avgX, y: -avgY)
+        }
     }
     
+    // runs when face changes
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let faceAnchor = anchor as? ARFaceAnchor, let faceGeometry = node.geometry as? ARSCNFaceGeometry {
+            faceGeometry.update(from: faceAnchor.geometry)
+            
+            leftEye.simdTransform = faceAnchor.leftEyeTransform
+            rightEye.simdTransform = faceAnchor.rightEyeTransform
+            
+            averageDistance()
+            hitTest()
+        }
+    }
+
     // MARK: - ARSessions
     
     func session(_ session: ARSession, didFailWithError error: Error) {

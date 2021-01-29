2e24d998f4f163271aeddc16fa5965e6c0c63a5a
Moa <moa.winberg.1@gmail.com>
2021-01-29 09:50:30 +0100

    clean up

--- eye-tracking/ViewController.swift
+++ eye-tracking/ViewController.swift
@@ -10,32 +10,6 @@ import SceneKit
 import ARKit
 import WebKit
 
-extension CGFloat {
-
-    func clamped(to: ClosedRange<CGFloat>) -> CGFloat {
-        return to.lowerBound > self ? to.lowerBound
-            : to.upperBound < self ? to.upperBound
-            : self
-    }
-}
-
-struct Constants {
-
-    struct Device {
-        static let screenSize = CGSize(width: 0.0623908297, height: 0.135096943231532)
-        static let frameSize = CGSize(width: 375, height: 812)
-    }
-
-    struct Ranges {
-        static let widthRange: ClosedRange<CGFloat> = (0...CGFloat(414))
-        static let heightRange: ClosedRange<CGFloat> = (0...CGFloat(896))
-    }
-
-    struct Colors {
-        static let crosshairColor: UIColor = .white
-    }
-}
-
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - outlets
@@ -54,19 +28,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     var screenPointsX: [CGFloat] = []
     var screenPointsY: [CGFloat] = []
-    
-    // Set target at 2 meters away from the center of eyeballs to create segment vector
-    var leftEyeEnd: SCNNode = {
-        let node = SCNNode()
-        node.position.z = 2
-        return node
-    }()
-    
-    var rightEyeEnd: SCNNode = {
-        let node = SCNNode()
-        node.position.z = 2
-        return node
-    }()
     
     var leftEye: SCNNode = {
         let geometry = SCNCone(topRadius: 0.005, bottomRadius: 0, height: 0.2)
@@ -93,16 +54,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         parentNode.addChildNode(node)
         return parentNode
     }()
-
-    var virtualPhoneNode: SCNNode = {
-        let geometry = SCNPlane(width: 1, height: 1)
-        geometry.firstMaterial?.isDoubleSided = true
-        geometry.firstMaterial?.diffuse.contents = UIColor.blue
-        let node = SCNNode()
-        node.geometry = geometry
-        node.position.z = 1
-        return node
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -110,8 +61,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         sceneView.automaticallyUpdatesLighting = true
         UIApplication.shared.isIdleTimerDisabled = true
-
-        sceneView.pointOfView?.addChildNode(virtualPhoneNode)
         
         // set camera to center of screen
         sceneView.pointOfView?.position.x = 207.0
@@ -140,28 +89,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let node = SCNNode(geometry: faceMesh)
         node.geometry?.firstMaterial?.fillMode = .lines
         
-        // add nodes to face
         node.addChildNode(leftEye)
         node.addChildNode(rightEye)
-        
-        // add target to eyes
-        leftEye.addChildNode(leftEyeEnd)
-        rightEye.addChildNode(rightEyeEnd)
         
         return node
     }
     
     func averageDistance() {
-        // Distance of the eyes to the camera don't have to subtract because the center of worldPosition (camera) is (0,0,0)
-        let distanceL = leftEye.worldPosition
-        let distanceR = rightEye.worldPosition
+        // euqludian distance of the eyes to the camera don't have to subtract because the center of worldPosition (camera) is (0,0,0)
+        let distanceL = sqrt(leftEye.worldPosition.x*leftEye.worldPosition.x + leftEye.worldPosition.y*leftEye.worldPosition.y + leftEye.worldPosition.z*leftEye.worldPosition.z)
+        let distanceR = sqrt(rightEye.worldPosition.x*rightEye.worldPosition.x + rightEye.worldPosition.y*rightEye.worldPosition.y + rightEye.worldPosition.z*rightEye.worldPosition.z)
         
-        // euqludian distance
-        let distanceLLength = sqrt(distanceL.x*distanceL.x + distanceL.y*distanceL.y + distanceL.z*distanceL.z)
-        let distanceRLength = sqrt(distanceR.x*distanceR.x + distanceR.y*distanceR.y + distanceR.z*distanceR.z)
+        let distance = (distanceL + distanceR) / 2
         
-        let distance = (distanceLLength + distanceRLength) / 2
-
         DispatchQueue.main.async{
             self.distanceLabel.text = "\(Int(round(distance * 100))) cm"
         }
@@ -170,41 +110,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func smoothing() {
         let threshold = 5
         
+        // limit number of points in array to threhold
         let pointsX = screenPointsX.suffix(threshold)
         let pointsY = screenPointsY.suffix(threshold)
         
-        var sumX = CGFloat(0);
-        for point in pointsX {
-           sumX += point
-        }
+        // get average point in array
+        let avgX =  pointsX.reduce(0, +) / CGFloat(pointsX.count)
+        let avgY = pointsY.reduce(0, +) / CGFloat(pointsY.count)
         
-        var sumY = CGFloat(0);
-        for point in pointsY {
-           sumY += point
-        }
-        
-        var avgX = sumX / CGFloat(pointsX.count)
-        var avgY = sumY / CGFloat(pointsY.count)
-        
-        avgX = avgX.clamped(to: Constants.Ranges.widthRange)
-        avgY = avgY.clamped(to: Constants.Ranges.heightRange)
+        // clamp point to screen size
+        let pointX = min(max(avgX, 0), CGFloat(phonePointsWidth))
+        let pointY = min(max(avgY, 0), CGFloat(phonePointsHeight))
         
         DispatchQueue.main.async {
-           // self.gazeIndicator.center.x = CGFloat(avgX)
-            self.gazeIndicator.center.y = CGFloat(avgY)
-            // self.gazeIndicator.center = CGPoint(x: CGFloat(p_raster_x), y: CGFloat(p_raster_y))
+            self.gazeIndicator.center.x = CGFloat(pointX)
+            // self.gazeIndicator.center.y = CGFloat(pointY)
+            // self.gazeIndicator.center = CGPoint(x: CGFloat(pointX), y: CGFloat(pointY))
         }
     }
     
     func rasterization() {
-        var p_world_right = SCNVector4(x: rightEye.worldPosition.x, y: rightEye.worldPosition.y, z: rightEye.worldPosition.z, w: 1)
-        var p_world_left = SCNVector4(x: leftEye.worldPosition.x, y: leftEye.worldPosition.y, z: leftEye.worldPosition.z, w: 1)
-        
+        // create nodes to update position
         let rightEyeNode: SCNNode = {
             let node = SCNNode()
-            node.position.x = rightEye.worldPosition.x
-            node.position.y = rightEye.worldPosition.y
-            node.position.z = rightEye.worldPosition.z
+            node.position = rightEye.worldPosition
             let parentNode = SCNNode()
             parentNode.addChildNode(node)
             return parentNode
@@ -212,9 +141,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         let leftEyeNode: SCNNode = {
             let node = SCNNode()
-            node.position.x = leftEye.worldPosition.x
-            node.position.y = leftEye.worldPosition.y
-            node.position.z = leftEye.worldPosition.z
+            node.position = leftEye.worldPosition
             let parentNode = SCNNode()
             parentNode.addChildNode(node)
             return parentNode
@@ -223,12 +150,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         rightEyeNode.simdTransform = rightEye.simdTransform
         leftEyeNode.simdTransform = leftEye.simdTransform
         
+        // find point in local coordinate system
+        let p_world_right = SCNVector4(x: rightEye.worldPosition.x, y: rightEye.worldPosition.y, z: rightEye.worldPosition.z, w: 1)
+        let p_world_left = SCNVector4(x: leftEye.worldPosition.x, y: leftEye.worldPosition.y, z: leftEye.worldPosition.z, w: 1)
+        
         let matrix_world_to_local_right = rightEye.simdTransform
         let matrix_world_to_local_left = leftEye.simdTransform
         
         let p_local_right = SIMD4<Float>(p_world_right) * matrix_world_to_local_right.inverse
         let p_local_left = SIMD4<Float>(p_world_left) * matrix_world_to_local_left.inverse
         
+        // find point in camera system
         let camera = sceneView.session.currentFrame?.camera
         let p_camera_right = SIMD4<Float>(p_local_right) * camera!.viewMatrix(for: .portrait).inverse
         let p_camera_left = SIMD4<Float>(p_local_left) * camera!.viewMatrix(for: .portrait).inverse
@@ -239,13 +171,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let p_y_right = p_camera_right.y / -p_camera_right.z
         let p_y_left = p_camera_left.y / -p_camera_left.z
         
+        // get average point of both eyes
         let p_x = (p_x_right + p_x_left) / 2
         let p_y = (p_y_right + p_y_left) / 2
         
+        // check if point is visible
         if abs(CGFloat(p_x)) <= phoneWidth/2 || abs(CGFloat(p_y)) <= phoneHeight/2 {
+            // Normalized Device Coordinate system. Remaps the point's coordinates in the range [0,1]
             let p_normalized_x  = (CGFloat(p_x) + phoneWidth/2) / phoneWidth
             let p_normalized_y  = (CGFloat(p_y) + phoneHeight/2) / phoneHeight
             
+            // define coordinates in raster space
             let p_raster_x = floor(p_normalized_x * phonePixelWidth)
             let p_raster_y = floor(p_normalized_y * phonePixelHeight)
             
